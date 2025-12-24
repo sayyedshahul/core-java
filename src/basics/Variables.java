8812309f77e5dbcc40642062c4c282c88205dbbc
Shahul Sayyed <sayyedshahul279@gmail.com>
2025-12-23 21:14:31 -0800

    Introduction to variables

---
+++ src/basics/Variables.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Variables {
+    public static void main(String[] args){
+        int num1 = 4;
+        int num2 = 17;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
