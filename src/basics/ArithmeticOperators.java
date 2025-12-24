544a60fd82e04a55fe43606ad2551629e3158b98
Shahul Sayyed <sayyedshahul279@gmail.com>
2025-12-24 01:02:14 -0800

    05 - Arithmetic operators in Java

---
+++ src/basics/ArithmeticOperators.java
@@ -0,0 +1,50 @@
+package basics;
+
+public class ArithmeticOperators {
+    public static void main(String[] args){
+        System.out.println("----With longform operator----");
+        int num1 = 7;
+        int num2 = 3;
+        int result;
+
+        result =  num1 + num2;
+        System.out.println("Addition --> " + result);
+
+        result =  num1 - num2;
+        System.out.println("Subtraction --> " + result);
+
+        result =  num1 * num2;
+        System.out.println("Multiplication --> " + result);
+
+        result =  num1 / num2;
+        System.out.println("Division --> " + result);
+
+        result =  num1 % num2;
+        System.out.println("Modulo --> " + result);
+
+        System.out.println("----With shorthand operator----");
+        int a = 8;
+        int b = 4;
+
+        a += b;
+        System.out.println("Addition --> " + a);
+
+        a -= b;
+        System.out.println("Subtraction --> " + a);
+
+        a *= b;
+        System.out.println("Multiplication --> " + a);
+
+        a /= b;
+        System.out.println("Division --> " + a);
+
+        a %= b;
+        System.out.println("Modulo --> " + a);
+
+        result = num1--;
+        System.out.println("Post Decrement --> " + result);
+
+        result = --num1;
+        System.out.println("Pre Decrement --> " + result);
+    }
+}
