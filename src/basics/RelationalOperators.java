88df888b3abe9a4cecd42ea4aef4386777036b7b
Shahul Sayyed <sayyedshahul279@gmail.com>
2025-12-24 01:20:54 -0800

    06 - Relational operators in Java

---
+++ src/basics/RelationalOperators.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class RelationalOperators {
+    public static void main(String[] args){
+        int n1 = 2;
+        int n2 = 5;
+        boolean result;
+
+        result = n1 > n2;
+        System.out.println(result);
+        result = n1 < n2;
+        System.out.println(result);
+        result = n1 == n2;
+        System.out.println(result);
+        result = n1 >= n2;
+        System.out.println(result);
+        result = n1 <= n2;
+        System.out.println(result);
+        result = n1 != n2;
+        System.out.println(result);
+    }
+}
