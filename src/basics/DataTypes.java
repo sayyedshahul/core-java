6a87c18c7b491be4b8968735fabeb48856d62665
Shahul Sayyed <sayyedshahul279@gmail.com>
2025-12-23 21:35:09 -0800

    02 - Introduction to Data Types in Java

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class DataTypes {
+    public static void main(String[] args){
+        // Integer subtypes
+        int num1 = 4;
+        short sh = 31234;
+        byte by =  125;
+        long lng = 8368288L;
+
+        //Float subtypes
+        float temperature = 43.76f;
+        double temp = 32.987;
+
+        //For single char values.
+        char c = 'A';
+
+        //For true or false values.
+        boolean condition = true;
+    }
+}
