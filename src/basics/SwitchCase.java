da908aa41c50d03d91d202db3f309841563a9804
Shahul Sayyed <sayyedshahul279@gmail.com>
2025-12-26 09:09:08 +0530

    10 - Switch Statement in Java

---
+++ src/basics/SwitchCase.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class SwitchCase {
+    public static void main(String[] args){
+        int month = 5;
+        switch(month){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            default:
+                System.out.println("Months after May");
+        }
+    }
+}
