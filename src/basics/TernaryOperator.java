37ecc392b0588f72ecb7a072e87968db04d00a0d
Shahul Sayyed <sayyedshahul279@gmail.com>
2025-12-26 08:47:37 +0530

    09 - Ternary Operator in Java

---
+++ src/basics/TernaryOperator.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class TernaryOperator {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 % num2 == 0 ? 1 : 0; //condition?true_value:false_value
+        System.out.println(result);
+    }
+}
