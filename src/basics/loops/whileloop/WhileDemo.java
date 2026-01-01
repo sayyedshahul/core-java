fa0bc8697ac32b11f1e5ca5a4e8abe5eefd37cfa
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-01 10:59:42 +0530

    11 - While loop in Java

---
+++ src/basics/loops/whileloop/WhileDemo.java
@@ -0,0 +1,16 @@
+package basics.loops.whileloop;
+
+public class WhileDemo {
+    public static void main(String[] args){
+        int i = 1;
+        while(i <= 5){
+            System.out.println("Hi "+ i);
+            i++;
+            int j = 1;
+            while(j <= 3){
+                System.out.println("Hello " + j);
+                j++;
+            }
+        }
+    }
+}
