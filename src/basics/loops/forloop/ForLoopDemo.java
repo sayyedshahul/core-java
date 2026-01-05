02fcaf5bf123f742bf05e1c7623ca11baba8150e
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-06 02:59:14 +0530

    13 - For Loop in Java

---
+++ src/basics/loops/forloop/ForLoopDemo.java
@@ -0,0 +1,12 @@
+package basics.loops.forloop;
+
+public class ForLoopDemo {
+    public static void main(String[] args){
+        for(int i=1; i<=7; i++){
+            System.out.println("DAY " + i);
+            for(int j=1; j<=9; j++){
+                System.out.println("  " + (j+8) + " - " + (j + 9));
+            }
+        }
+    }
+}
