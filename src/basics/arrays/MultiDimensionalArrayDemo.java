3319d34a64a96ef8d75bb3e18f88761bc326f2fb
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-06 14:58:31 +0530

    17 - Multi-Dimensional Arrays in Java

---
+++ src/basics/arrays/MultiDimensionalArrayDemo.java
@@ -0,0 +1,20 @@
+package basics.arrays;
+
+public class MultiDimensionalArrayDemo {
+    public static void main(String[] args){
+        int[][] nums = new int[3][3];
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums[i].length; j++){
+                nums[i][j] = (int)(Math.random() * 1000);
+            }
+        }
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums[i].length; j++){
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
