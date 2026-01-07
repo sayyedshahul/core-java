494306fbc06f1779c4097693c4eac165d0f9c1c0
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-07 14:53:22 +0530

    26 - Encapsulation in Java

---
+++ src/basics/oop/Encapsulation.java
@@ -0,0 +1,31 @@
+package basics.oop;
+
+class Hotel{
+    private double earnings;
+    private String name;
+
+    public double getEarnings(){
+        return earnings;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setEarnings(double earnings){
+        this.earnings = earnings;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args){
+        Hotel h1 = new Hotel();
+        h1.setEarnings(323_244_243.21);
+        h1.setName("Taj Hotel");
+        System.out.println(h1.getName() + " --> " + h1.getEarnings());
+    }
+}
---
+++ src/basics/oop/Encapsulation2.java
@@ -0,0 +1,30 @@
+package basics.oop;
+
+class Car{
+    private String engine;
+    private int modelNo;
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public int getModelNo() {
+        return modelNo;
+    }
+
+    public void setModelNo(int modelNo) {
+        this.modelNo = modelNo;
+    }
+}
+
+public class Encapsulation2 {
+    public static void main(String[] args){
+        Car mercedes = new Car();
+        mercedes.setEngine("V8");
+        mercedes.setModelNo(234);
+    }
+}
