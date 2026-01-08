6c5796be5d1facb92182cc74ee75f291cf4d9d4c
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-08 14:41:09 +0530

    36 - Final class in Java

--- src/basics/oop/FinalKeywordDemo.java
+++ src/basics/oop/FinalKeywordDemo.java
@@ -1,5 +1,21 @@
 package basics.oop;
 
+final class Calc{ //This would prevent subclass creation.
+    public void show(){
+        System.out.println("In show");
+    }
+    public int add(int a, int b){
+        return a + b;
+    }
+}
+
+/*
+class AdvCalc extends Calc{
+
+}
+Not possible as Calc is final
+*/
+
 public class FinalKeywordDemo {
     public static void main(String[] args){
         final int num = 9;
