0237fdaa1e2be1a801d8ab96e87f51b9a294b2b1
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-08 18:46:35 +0530

    42 - Auto-boxing and Auto-unboxing

--- src/basics/oop/WrapperClasses.java
+++ src/basics/oop/WrapperClasses.java
@@ -2,7 +2,23 @@ package basics.oop;
 
 public class WrapperClasses {
     public static void main(String[] args){
-        Integer num = new Integer(4);
+        Integer num = new Integer(4); //Boxing
         System.out.println(num);
+
+        Integer num1 = 9; // Auto-boxing
+        System.out.println(num1);
+
+        int num3 = num1.intValue(); // Un-boxing
+        System.out.println(num3);
+
+        int num4 = num1; // Un-boxing
+        System.out.println(num4);
+
+        Double d = 23.45;
+        Character c = 'A';
+
+        String str = "12";
+        int result = Integer.parseInt(str) * 2;
+        System.out.println(result);
     }
 }
