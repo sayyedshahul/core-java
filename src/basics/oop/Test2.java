96118f6797f978663092bfa45ba6e7e3a4f9a3b1
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-08 13:49:35 +0530

    Experimented with class access modifiers

---
+++ src/basics/oop/Test2.java
@@ -0,0 +1,7 @@
+package basics.oop;
+import basics.oop.accessmodifiers.Test;
+
+public class Test2 {
+    public static void main(String[] args){
+    }
+}
---
+++ src/basics/oop/accessmodifiers/Test.java
@@ -0,0 +1,7 @@
+package basics.oop.accessmodifiers;
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Running test...");
+    }
+}
