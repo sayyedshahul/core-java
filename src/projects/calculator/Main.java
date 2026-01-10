aabfaf7740d21c5af40993f1395ca3962d9b44c2
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-10 14:15:20 +0530

    Handled invalid operation menu input

--- src/projects/calculator/Main.java
+++ src/projects/calculator/Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args){
         double num1;
         double num2;
-        int operation;
-        Scanner scn = new Scanner(System.in);
+
+        int operation = 0;
         char isContinue = 'y';
+
+        Scanner scn = new Scanner(System.in);
         Calculator calc = new Calculator();
 
         while(isContinue == 'y') {
@@ -16,17 +18,24 @@ public class Main {
             num1 = scn.nextDouble();
             System.out.print("Input number 2: ");
             num2 = scn.nextDouble();
+
             calc.setNum1(num1);
             calc.setNum2(num2);
 
             calc.showMenu();
             operation = scn.nextInt();
-            if(operation == 4 && num2 == 0){
-                System.out.println("result = Undefined(Divide by zero)");
+            while(operation < 1 || operation > 4){
+                System.out.println("Invalid operation.");
+                calc.showMenu();
+                operation = scn.nextInt();
             }
-            else {
+
+            if (operation == 4 && num2 == 0) {
+                System.out.println("result = Undefined(Divide by zero)");
+            } else {
                 System.out.println("result = " + calc.performOperation(operation));
             }
+
             System.out.print("Do you want to continue(y/n): ");
             isContinue = scn.next().charAt(0);
         }
