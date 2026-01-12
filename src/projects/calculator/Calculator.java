28848c34e66c69fdd8ad6d0227ad475d9bf94cad
Shahul Sayyed <sayyedshahul279@gmail.com>
2026-01-12 10:51:49 +0530

    Added operation history feature and fixed division method to calculator project

--- src/projects/calculator/Calculator.java
+++ src/projects/calculator/Calculator.java
@@ -1,8 +1,17 @@
 package projects.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
     private double num1;
     private double num2;
+    private List<String> history = new ArrayList<>();
+    private final int n = 5; // Max history entries.
+
+    public List<String> getHistory(){
+        return new ArrayList<>(history);
+    }
 
     public double getNum1(){
         return num1;
@@ -26,7 +35,14 @@ public class Calculator {
         System.out.println("2. Subtraction");
         System.out.println("3. Multiplication");
         System.out.println("4. Division");
+        System.out.println("5. Show history");
         System.out.print("--> ");
+    }
+
+    public void showHistory(){
+        for(String historyEntry: history){
+            System.out.println(historyEntry);
+        }
     }
 
     public double add(double num1, double num2){
@@ -42,17 +58,44 @@ public class Calculator {
     }
 
     public double div(double num1, double num2){
+        if(num2 == 0){
+            throw new IllegalArgumentException("Can't divide by zero");
+        }
         double result = (double) num1 / num2;
-        return Math.round(result * 100) / 100.0;
+        return Math.round(result * 100) / 100.0; // To round answer to two decimal places.
     }
 
     public double performOperation(int operation){
-        return switch(operation){
-            case 1 -> add(num1, num2);
-            case 2 -> subtract(num1, num2);
-            case 3 -> multiply(num1, num2);
-            case 4 -> div(num1, num2);
+        char operand = '+';
+        String historyEntry;
+
+        double result = switch(operation){
+            case 1 -> {
+                operand = '+';
+                yield add(num1, num2);
+            }
+            case 2 -> {
+                operand = '-';
+                yield subtract(num1, num2);
+            }
+            case 3 -> {
+                operand = '*';
+                yield multiply(num1, num2);
+            }
+            case 4 ->{
+                operand = '/';
+                yield div(num1, num2);
+            }
             default -> 0;
         };
+
+        historyEntry = num1 + " " + operand + " " + num2 + " = " +  result;
+
+        if(history.size() == n){
+            history.remove(0);
+        }
+        history.add(historyEntry);
+
+        return result;
     }
 }
--- src/projects/calculator/Main.java
+++ src/projects/calculator/Main.java
@@ -1,44 +1,59 @@
 package projects.calculator;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         double num1;
-        double num2;
+        double num2 = 0;
+        double result;
 
-        int operation = 0;
-        char isContinue = 'y';
+        int operation = 0; // Menu option number for selected operation.
+        boolean continueCalculating = true; // Choice to continue next calculation.
+        boolean useFreshOperands = true; // Take fresh operands.
 
         Scanner scn = new Scanner(System.in);
         Calculator calc = new Calculator();
 
-        while(isContinue == 'y') {
-            System.out.print("Input number 1: ");
-            num1 = scn.nextDouble();
-            System.out.print("Input number 2: ");
-            num2 = scn.nextDouble();
-
-            calc.setNum1(num1);
-            calc.setNum2(num2);
-
+        while(continueCalculating) {
             calc.showMenu();
             operation = scn.nextInt();
-            while(operation < 1 || operation > 4){
+            while (operation < 1 || operation > 5) {
                 System.out.println("Invalid operation.");
                 calc.showMenu();
                 operation = scn.nextInt();
             }
 
-            if (operation == 4 && num2 == 0) {
-                System.out.println("result = Undefined(Divide by zero)");
+            if (operation == 5) {
+                calc.showHistory();
             } else {
-                System.out.println("result = " + calc.performOperation(operation));
+                if (!calc.getHistory().isEmpty()) {
+                    System.out.print("Do you want to enter new operands(y/n): ");
+                    useFreshOperands = scn.next().toLowerCase().startsWith("y");
+                }
+
+                if (useFreshOperands) {
+                    System.out.print("Input number 1: ");
+                    num1 = scn.nextDouble();
+                    System.out.print("Input number 2: ");
+                    num2 = scn.nextDouble();
+
+                    calc.setNum1(num1);
+                    calc.setNum2(num2);
+                }
+
+                try {
+                    result = calc.performOperation(operation);
+                    System.out.println("result = " + result);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("result = " + e);
+                }
             }
 
             System.out.print("Do you want to continue(y/n): ");
-            isContinue = scn.next().charAt(0);
+            continueCalculating = scn.next().toLowerCase().startsWith("y");
         }
-
+        scn.close();
     }
 }
